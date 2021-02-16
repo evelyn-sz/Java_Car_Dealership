c6704b83aa7bc9abc5a21dd06032b8a3c4ec014f
evelyn-sz <evelynszarek@gmail.com>
2021-02-16 13:45:30 +0000

    Tyres have a brand

--- src/test/java/TyreTest.java
+++ src/test/java/TyreTest.java
@@ -1,3 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class TyreTest {
     Tyre tyre;
+
+    @Before
+    public void setUp(){
+        tyre = new Tyre("Michelin");
+    }
+
+    @Test
+    public void hasBrand() {
+        assertEquals("Michelin", tyre.getBrand());
+    }
 }
