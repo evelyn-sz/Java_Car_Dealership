f8a6bb4c60e5e159e1d07e6b209a8a4373e6fe86
evelyn-sz <evelynszarek@gmail.com>
2021-02-16 13:52:07 +0000

    Customer hasMoney and hasCollectionOfCars

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Customer {
+
+    private double money;
+    private ArrayList<Car> carCollection;
+
+    public Customer(double money) {
+        this.money = money;
+        this.carCollection = new ArrayList<>();
+    }
+
+    public double getMoney() {
+        return this.money;
+    }
+
+    public int getNumberOfOwnedCars() {
+        return this.carCollection.size();
+    }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,3 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
     Customer customer;
+
+    @Before
+    public void setUp(){
+        customer = new Customer(1000.00);
+    }
+
+    @Test
+    public void hasMoney(){
+        assertEquals(1000.00, customer.getMoney(), 0.0);
+    }
+
+    @Test
+    public void carCollectionStartsEmpty(){
+        assertEquals(0, customer.getNumberOfOwnedCars());
+    }
 }
